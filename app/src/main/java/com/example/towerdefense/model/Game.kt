52b08d973fc4917f155002edfe47e6e8a64cb33f
Krystof <krystof.cotestjacques@ulb.be>
2024-04-01 07:58:52 -0400

    Ajoute singleton+modifications dans Game, Map, GameMap

--- app/src/main/java/com/example/towerdefense/model/Game.kt
+++ app/src/main/java/com/example/towerdefense/model/Game.kt
@@ -6,16 +6,15 @@ class Game {
     private var tickDuration : Long = 500
     private var money : Int = 1000
     private var hitPoints : Int = 200
-    private val nWave = 12
+
 
     private val gameManager: GameManager = GameManager()
     private var gameTimer : GameTimer? = null
-    private val waves: MutableList<Wave> = mutableListOf()
+
 
     //*************************************** Constructor *************************************** //
     init {
         initGameTimer()
-        initWaves()
     }
 
     private fun initGameTimer() {
@@ -23,16 +22,13 @@ class Game {
         gameTimer?.setTickListener { this.advanceTick() }
     }
 
-    private fun initWaves() {
-        for (i in 0 until nWave)
-            waves.add(Wave(i))
-    }
-
     //************************************* Public methods ************************************* //
     fun startWave() {
         // !!!SB: Appeler la fonction à partir d'eventHandler buttons. À compléter
-        if (currentWave < nWave) {
-            gameManager.setPendingEnemies(waves[currentWave].getWaveEnemies())
+
+        //!!!KC: Changer pour accéder au singleton (nWave)
+        if (currentWave < GameMap.getMap().nWave) {
+            gameManager.setPendingEnemies(GameMap.getWaveEnemies(currentWave))
             gameTimer?.start()
         }
     }
--- app/src/main/java/com/example/towerdefense/model/GameMap.kt
+++ app/src/main/java/com/example/towerdefense/model/GameMap.kt
@@ -2,63 +2,44 @@ package com.example.towerdefense.model
 
 import kotlin.math.roundToInt
 
-class GameMap {
-    //**************************************** Variables **************************************** //
-    val nRows: Int = 8
-    val nColumns: Int = 16
+object GameMap {
 
-    val emptyTile = 0
-    val towerTile = -1
-
-    val pxPerTile = 120
-
-    // Encoding of the tile which contains a road (grid x, grid y)
-    private val firstTile : Pair<Int, Int> = Pair(3,0)
-
-    // Absolut path: 0 = right, 1 = up, 2 = left, 3 = down
-    private val pathEncoding = listOf(
-        3,3,0,0,3,3,2,2,3,3,0,0,0,0,1,1,1,1,1,0,0,0,0,0,3,3,2,2,3,3,0,0,3,3
-    )
-
-    val mapGrid  = MutableList(nColumns) { MutableList(nRows) { emptyTile } }
+    //*************************************** Variables ***************************************** //
+    private var map : Map = Map()
 
     //*************************************** Constructor *************************************** //
-    init {
-        var col = firstTile.first
-        var row = firstTile.second
-
-        mapGrid[col][row] = 1
-
-        for (i in pathEncoding.indices) {
-            when (pathEncoding[i]){
-                0 -> mapGrid[++col][row] = i + 2
-                1 -> mapGrid[col][--row] = i + 2
-                2 -> mapGrid[--col][row] = i + 2
-                3 -> mapGrid[col][++row] = i + 2
-            }
-        }
-    }
 
 
     //************************************* Public methods ************************************** //
     fun gridToPixel(gridX: Int, gridY: Int): Pair<Int, Int>{
-        val px = (gridX * pxPerTile + 0.5 * pxPerTile).roundToInt()
-        val py = (gridY * pxPerTile + 0.5 * pxPerTile).roundToInt()
+        val px = (gridX * map.pxPerTile + 0.5 * map.pxPerTile).roundToInt()
+        val py = (gridY * map.pxPerTile + 0.5 * map.pxPerTile).roundToInt()
         return Pair(px, py)
+
     }
 
     fun pixelToGrid(px : Int, py: Int): Pair<Int, Int>{
-        val gridX = (px.floorDiv(pxPerTile))
-        val gridY = (py.floorDiv(pxPerTile))
+        val gridX = (px.floorDiv(map.pxPerTile))
+        val gridY = (py.floorDiv(map.pxPerTile))
         return Pair(gridX,gridY)
     }
 
     fun isEmptyTile(gridX: Int, gridY: Int) : Boolean {
-        return mapGrid[gridX][gridY] == emptyTile
+        return map.mapGrid[gridX][gridY] == map.emptyTile
     }
 
     fun isTowerTile(gridX: Int, gridY: Int) : Boolean {
-        return mapGrid[gridX][gridY] == towerTile
+        return map.mapGrid[gridX][gridY] == map.towerTile
+    }
+
+    fun loadMap(mapToLoad:Map){
+        map = mapToLoad
+    }
+
+    fun getMap() : Map { return this.map }
+
+    fun getWaveEnemies(nWave: Int): MutableList<Enemy>{
+        return map.getWaves()[nWave].getWaveEnemies()
     }
 
     //************************************* Private methods ************************************* //
