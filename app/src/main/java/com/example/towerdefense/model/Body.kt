f90ada340891147487527124937d1d44d59044a2
Simon Beaulieu <simonbeaulieu9@outlook.com>
2024-04-15 17:01:26 +0200

    Ajout fonction move dans Enemy: trouver prochaine case route

--- app/src/main/java/com/example/towerdefense/model/Body.kt
+++ app/src/main/java/com/example/towerdefense/model/Body.kt
@@ -2,24 +2,21 @@ package com.example.towerdefense.model
 
 abstract class Body(col : Int, row : Int) {
     //**************************************** Variables **************************************** //
-    // !!!SB: Attention au valeurs "hard codé", c'est correct ne pas en mettre aussi de limite
-    protected var posX: Int = 0
+    protected var gridX: Int = col
         set(value) {
-            if (value in 0..2080)
+            if (GameMap.isValidCol(value)) {
                 field = value
+            }
         }
-    protected var posY: Int = 0
+    protected var gridY: Int = row
         set(value) {
-            if (value in 0..1080)
-                field=value
+            if (GameMap.isValidRow(value)) {
+                field = value
+            }
         }
 
     //*************************************** Constructor *************************************** //
-    init {
-        val p = GameMap.gridToPixel(col, row)
-        posX = p.first
-        posY = p.second
-    }
+
 
     //************************************* Public methods ************************************* //
     abstract fun advanceTick()
